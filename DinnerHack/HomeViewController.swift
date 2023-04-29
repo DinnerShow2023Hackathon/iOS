2c41f48a3a81f102646a2cc43541dd7d5790e358
oy6uns <bacho4235@gmail.com>
2023-04-29 22:58:01 +0900

    [Add] Font 추가

--- DinnerHack/HomeViewController.swift
+++ DinnerHack/HomeViewController.swift
@@ -9,29 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    private let nextBTN: UIButton = {
-        $0.backgroundColor = .orange
-        $0.setTitle("다음", for: .normal)
-        $0.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        $0.setTitleColor(.white, for: .normal)
-        $0.addTarget(self, action: #selector(tapNextBTN), for: .touchUpInside)
-        return $0
-    }(UIButton())
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
- 
-        self.view.addSubview(nextBTN)
-        nextBTN.anchor(
-            left: view.leftAnchor,
-            bottom: view.bottomAnchor,
-            right: view.rightAnchor,
-            paddingBottom: 40,
-            height: 40
-        )
 
     }
     
