dc8b055842b320cb572b9443e4883ab66d7a4f38
oy6uns <bacho4235@gmail.com>
2023-04-30 05:39:34 +0900

    [Feat] Network 구조 구현

---
+++ DinnerHack/Network/Encodable+.swift
@@ -0,0 +1,23 @@
+//
+//  Encodable+.swift
+//  DinnerHack
+//
+//  Created by saint on 2023/04/30.
+//
+
+import Foundation
+
+// MARK: - Encodable Extension
+
+extension Encodable {
+    
+  func asParameter() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            as? [String: Any] else {
+      throw NSError()
+    }
+    
+    return dictionary
+  }
+}
