dee35b90edaeec8b63fd211c6daea4943f653c11
Dinner <junghwan3588@gm.gist.ac.kr>
2023-04-29 13:22:57 +0900

    First Setting

---
+++ DinnerHack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DinnerHack
+//
+//  Created by creohwan on 2023/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
